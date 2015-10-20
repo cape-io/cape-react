66c1887a919195a0cd52963dac74b71eb130dd30
Kai Curry <kai@sundaysenergy.com>
2015-10-20 13:22:31 -0500

    Put Login component as root. Close #1

---
+++ src/routes.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { IndexRoute, Route } from 'react-router';
+import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
+import {
+    App,
+    About,
+    Login,
+    LoginSuccess,
+    Mixer,
+    NotFound,
+  } from './containers';
+import Logout from './components/Logout';
+
+export default (store) => {
+  const requireLogin = (nextState, replaceState, cb) => {
+    function checkAuth() {
+      const { auth: { user }} = store.getState();
+      if (!user) {
+        // oops, not logged in, so can't be here!
+        replaceState(null, '/login');
+      }
+      cb();
+    }
+
+    if (!isAuthLoaded(store.getState())) {
+      store.dispatch(loadAuth()).then(checkAuth);
+    } else {
+      checkAuth();
+    }
+  };
+
+  function handleLogout(nextState, replaceState, next) {
+    const { auth: { user }} = store.getState();
+    if (user) {
+      store.dispatch(logout());
+    } else {
+      // oops, not logged in, so can't be here!
+      replaceState(null, '/login');
+    }
+    next();
+  }
+  /**
+   * Please keep routes in alphabetical order
+   */
+  return (
+    <Route path="/" component={App}>
+      { /* Home (main) route */ }
+      <IndexRoute component={Login}/>
+
+      { /* Routes requiring login */ }
+      <Route onEnter={requireLogin}>
+        <Route path="mixer" component={Mixer} />
+        <Route path="loginSuccess" component={LoginSuccess} />
+      </Route>
+
+      { /* Routes */ }
+      <Route path="about" component={About} />
+      <Route path="login-join" component={Login} />
+      <Route onEnter={handleLogout} path="logout" component={Logout} />
+
+      { /* Catch all route */ }
+      <Route path="*" component={NotFound} status={404} />
+    </Route>
+  );
+};
