89a14368db215335d724b8ecbbc5929d15ebadef
Kai Curry <kai@sundaysenergy.com>
2015-10-20 13:31:21 -0500

    About placeholder

---
+++ src/containers/About/About.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import DocumentMeta from 'react-document-meta';
+
+export default class About extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1>About Us</h1>
+        <DocumentMeta title="CAPE: About Us"/>
+
+        <p>Some text from something about us.</p>
+
+      </div>
+    );
+  }
+}
