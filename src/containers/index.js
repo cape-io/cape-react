c48c55d192c932e894679c263dbad913e2410215
Kai Curry <kai@sundaysenergy.com>
2015-10-21 12:20:28 -0500

    mixer form container

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,4 +3,5 @@ export About from './About/About';
 export Login from './login';
 export LoginSuccess from './LoginSuccess/LoginSuccess';
 export Mixer from './mixer/mixer';
+export MixerForm from './mixer/form';
 export NotFound from './NotFound/NotFound';
