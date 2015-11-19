d1c4bdb447a7aaa049310aace82e0e5d07cfafbc
Kai Curry <kai@cape.io>
2015-11-19 15:29:30 -0600

    do not use local api

--- src/server.js
+++ src/server.js
@@ -29,32 +29,6 @@ app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
 
 app.use(Express.static(path.join(__dirname, '..', 'static')));
 
-if (__DEVELOPMENT__) {
-  const httpProxy = require('http-proxy');
-  const proxy = httpProxy.createProxyServer({
-    target: config.api,
-    // ws: true
-  });
-  // Proxy to API server
-  app.use('/api', (req, res) => {
-    proxy.web(req, res);
-  });
-
-  // Added the error handling to avoid https://github.com/nodejitsu/node-http-proxy/issues/527
-  proxy.on('error', (error, req, res) => {
-    let json;
-    if (error.code !== 'ECONNRESET') {
-      console.error('proxy error', error);
-    }
-    if (!res.headersSent) {
-      res.writeHead(500, {'content-type': 'application/json'});
-    }
-
-    json = {error: 'proxy_error', reason: error.message};
-    res.end(JSON.stringify(json));
-  });
-}
-
 app.use((req, res) => {
   if (__DEVELOPMENT__) {
     // Do not cache webpack stats: the script file would change since
