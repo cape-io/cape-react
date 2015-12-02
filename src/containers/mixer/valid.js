adea4fe3fcb1ea9ed8018a5750ec759115bcc1ae
Kai Curry <kai@cape.io>
2015-12-01 21:21:52 -0600

    cleanup

--- src/containers/mixer/valid.js
+++
@@ -1,9 +0,0 @@
-import memoize from 'lru-memoize';
-import {createValidator, isRequired, maxLength, isEmail} from 'utils/validation';
-
-const surveyValidation = createValidator({
-  fisrtName: [isRequired, maxLength(10)],
-  email: [isRequired, isEmail],
-  occupation: maxLength(20), // single rules don't have to be in an array
-});
-export default memoize(10)(surveyValidation);
