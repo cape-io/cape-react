3531398aba4db86ca4ea4556a80463dc38d57d7e
Kai Curry <kai@cape.io>
2016-02-05 15:19:24 -0600

    eslint

--- src/defaultState.js
+++ src/defaultState.js
@@ -1,21 +1,17 @@
-import acfProfile from './contentTypes/acfProfile';
 
 function createNavLink(id, args = {}) {
-  const {text, to, authenticated, rest} = args;
+  const { text, to, authenticated, rest } = args
   return {
     authenticated,
     id,
     text: text || id.charAt(0).toUpperCase() + id.slice(1),
-    to: to || '/' + id,
+    to: to || `/${id}`,
     ...rest,
-  };
+  }
 }
 
 export default {
   db: {
-    contentTypes: [
-      acfProfile,
-    ],
     support: {
       chat: 'https://www.hipchat.com/gv1XLjgaV',
       bugs: 'https://github.com/cape-io/cape-editor/issues',
@@ -34,10 +30,10 @@ export default {
     },
     // @TODO The server needs filter based on permissions besides auth.
     navLinks: [
-      createNavLink('about', {text: 'About Us'}),
-      createNavLink('mixer', {authenticated: true}),
-      createNavLink('login-join', {authenticated: false, text: 'Login or Join'}),
-      createNavLink('logout', {authenticated: true, className: 'logout-link'}),
+      createNavLink('about', { text: 'About Us' }),
+      createNavLink('mixer', { authenticated: true }),
+      createNavLink('login-join', { authenticated: false, text: 'Login or Join' }),
+      createNavLink('logout', { authenticated: true, className: 'logout-link' }),
     ],
   },
-};
+}
