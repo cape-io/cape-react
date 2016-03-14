6481a3aca89c79902b46387ad62e2674a096cd96
Kai Curry <kai@cape.io>
2016-03-14 13:57:10 -0500

    add a link to homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-
+import Link from 'redux-history-sync'
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
         <h2>Hello</h2>
+        <Link to="/mixer/">Mixer</Link>
       </div>
     )
   }
