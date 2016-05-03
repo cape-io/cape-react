5c7d838b91f586018c9000a872c9793364add312
Kai Curry <kai@cape.io>
2016-05-03 12:49:36 -0500

    rm redux/graph

--- src/node.js
+++ src/node.js
@@ -3,7 +3,6 @@ import { combineReducers } from 'redux'
 import * as reducer from './redux/reducer'
 
 export * as auth from './redux/auth'
-export * as graph from './redux/graph'
 
 export const capeReducer = combineReducers(reducer)
 export capeRoutes, { locationInfo as capeLocationInfo } from './redux/routes'
